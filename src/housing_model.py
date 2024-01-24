36d9ddb3e1bd97f51fdc775e45d4d74abb96f841
Ajith Sharma <ajiths@squareup.com>
2024-01-23 22:27:05 -0800

    Adding a streamlit App and exploratory analysis

--- src/housing_model.py
+++ src/housing_model.py
@@ -1,3 +1,5 @@
+import sys
+
 import joblib
 import pandas as pd
 import numpy as np
@@ -7,6 +9,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from lightgbm import LGBMRegressor
 
 
 # Load the dataset
@@ -16,26 +19,33 @@ def load_data():
     return df
 
 
+categorical_features = ['TYPE', 'ZIP_CODE', 'SUBLOCALITY']
+continuous_features = ['BEDS', 'BATH', 'PROPERTYSQFT']
+
+
 # Perform data transformations
 def perform_data_transformations(df, model_columns):
     # Add your data transformations here
     df['ZIP_CODE'] = df['STATE'].str.extract(r'(\d{5})')
     df = df[df.loc[:, "PRICE"] < 100000000]
+    for feature in categorical_features:
+        df[feature] = df[feature].astype('category')
     model_df = df.loc[:, model_columns]
     return model_df
 
 
 def encode_data(df, categorical_features, continuous_features):
     df_encoded = pd.get_dummies(df, columns=categorical_features, drop_first=True)
-    X = df_encoded[continuous_features + df_encoded.columns[df_encoded.columns.str.startswith('TYPE_')].tolist() +
-                   df_encoded.columns[df_encoded.columns.str.startswith('ZIP_CODE_')].tolist() + df_encoded.columns[
-                       df_encoded.columns.str.startswith('SUBLOCALITY_')].tolist()].astype(float)
+    X = df_encoded[
+        continuous_features + df_encoded.columns[df_encoded.columns.str.startswith('TYPE_')].tolist() +
+        df_encoded.columns[df_encoded.columns.str.startswith('ZIP_CODE_')].tolist() + df_encoded.columns[
+            df_encoded.columns.str.startswith('SUBLOCALITY_')].tolist()].astype(float)
     return X
 
 
 def scale_data(X):
     sqft_scaled = StandardScaler()
-    X['PROPERTYSQFT'] = sqft_scaled.fit_transform(X[['PROPERTYSQFT']])
+    X['PROPERTYSQFT'] = X.loc[:, 'PROPERTYSQFT'].sqft_scaled.fit_transform(X[['PROPERTYSQFT']])
     current_dir = os.path.dirname("/Users/ajiths/PycharmProjects/NYC_home_price_project/src")
     scaler_path = os.path.join(current_dir, 'scaler.pkl')
     joblib.dump(sqft_scaled, scaler_path)
@@ -65,6 +75,14 @@ def train_random_forest_model(X_train, y_train):
     return model
 
 
+# Train the LightGBM Regressor
+def train_lightgbm_regressor(X_train, y_train, model_path='light_gbm.pkl'):
+    lgbm_model = LGBMRegressor(num_leaves=31, learning_rate=0.05, n_estimators=100, objective='regression')
+    lgbm_model.fit(X_train, y_train)
+    joblib.dump(lgbm_model, model_path)
+    return lgbm_model
+
+
 # Evaluate the model
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
@@ -75,7 +93,7 @@ def evaluate_model(model, X_test, y_test):
 # Function to make predictions
 def predict_price(beds, bath, prop_sqft, prop_type, zip_code, sublocality, model_path):
     # Load the pre-trained model
-    rf_model = joblib.load(model_path)
+    model = joblib.load(model_path)
 
     # Load the pre-trained scaler
     # joblib.load('src/scaler.pkl')
@@ -90,21 +108,23 @@ def predict_price(beds, bath, prop_sqft, prop_type, zip_code, sublocality, model
         'SUBLOCALITY': [sublocality]
     })
 
-
-    # Encode and scale the input data
-    encoded_data = encode_data(input_data, categorical_features, continuous_features)
-    scaled_data = scale_data(encoded_data)
-
-
     # Make predictions
-    prediction = rf_model.predict(scaled_data)
+    for feature in categorical_features:
+        input_data[feature] = input_data[feature].astype('category')
+    prediction = model.predict(input_data)
     return prediction[0]
 
 
-# rf_model = joblib.load('/Users/ajiths/PycharmProjects/NYC_home_price_project/src/rf_model.pkl')
-# scaler = joblib.load('src/scaler.pkl')
+# Function to load the pre-trained model
+def load_model(model_path):
+    try:
+        model = joblib.load(model_path)
+        print(f"Model loaded successfully. Model type: {type(model)}")
+        return model
+    except Exception as e:
+        print(f"Error loading the model: {e}")
+        return None
 
-# Main function to orchestrate the workflow
 
 def main():
     # Load data
@@ -115,26 +135,29 @@ def main():
     # Perform data transformations
     model_columns = ['BEDS', 'BATH', 'PROPERTYSQFT', 'TYPE', 'ZIP_CODE', 'SUBLOCALITY', 'PRICE']
     df = perform_data_transformations(df, model_columns)
-    # Define categorical features for one-hot encoding
-    categorical_features = ['TYPE', 'ZIP_CODE', 'SUBLOCALITY']
-    continuous_features = ['BEDS', 'BATH', 'PROPERTYSQFT']
-    # One-hot encode categorical features
-    encoded_data = encode_data(df, categorical_features, continuous_features)
-    scaled_data = scale_data(encoded_data)
 
-    y = df['PRICE']
+    light_gbm_path = 'light_gbm.pkl'
 
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(scaled_data,y)
+    # Streamlit app
+    st.title('Home Price Prediction App')
+    #
+    # Input fields for user to enter property details
+    beds = st.slider('Number of Bedrooms', min_value=1, max_value=50, value=3)
+    bath = st.slider('Number of Bathrooms', min_value=1, max_value=50, value=2)
+    prop_sqft = st.slider('Property Square Footage', min_value=500, max_value=100000, value=2000)
+    prop_type = st.selectbox('Property Type', df['TYPE'].unique())
+    zip_code = st.text_input('Zip Code', 'Enter Zip Code')
+    sublocality = st.text_input('Sublocality', 'Enter Sub Locality')
 
-    # Train the Random Forest model
-    model = train_random_forest_model(X_train, y_train)
+    if st.button('Predict Price'):
 
-    # Evaluate the model
-    rmse = evaluate_model(model, X_test, y_test)
-    print(f'Root Mean Squared Error: {rmse}')
+        # Make predictions using the pre-trained model
+
+        prediction = predict_price(beds, bath, prop_sqft, prop_type, zip_code, sublocality, light_gbm_path)
+        st.success(f'Predicted Price: ${prediction:,.2f}')
 
 
 if __name__ == "__main__":
     main()
 
+#######################
